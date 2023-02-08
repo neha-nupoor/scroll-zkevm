scroll-tech/scroll-prover

for dynamic codehash feat

zkevm/src/circuit/builder.rs
@@ -1,8 +1,8 @@
 use bus_mapping::circuit_input_builder::{BlockHead, CircuitInputBuilder};
 
-use bus_mapping::state_db::{Account, CodeDB, StateDB};
+use bus_mapping::state_db::{Account, CodeDB, CodeHash, StateDB};
 use eth_types::evm_types::OpcodeId;
-use eth_types::ToAddress;
+use eth_types::{Hash, ToAddress};
 use ethers_core::types::{Address, Bytes, U256};
 
 use halo2_proofs::halo2curves::bn256::Fr;
@@ -16,8 +16,9 @@ use types::eth::{BlockResult, ExecStep};
 use zkevm_circuits::evm_circuit::table::FixedTableTag;
 
 use halo2_proofs::arithmetic::FieldExt;
-use mpt_circuits::hash::Hashable;
-use zkevm_circuits::evm_circuit::witness::{block_convert, Block, Bytecode};
+use mpt_circuits::hash::{Hashable, MessageHashable};
+use zkevm_circuits::evm_circuit::witness::{block_convert, Block};
+use zkevm_circuits::tx_circuit::PrimeField;
 
 use super::DEGREE;
 use anyhow::anyhow;
@@ -84,32 +85,6 @@ pub fn block_results_to_witness_block(
 
     let mut witness_block = block_convert(&builder.block, &builder.code_db);
     witness_block.evm_circuit_pad_to = (1 << *DEGREE) - 64;
-
-    // hack bytecodes table in witness
-    witness_block.bytecodes = builder
-        .block
-        .txs()
-        .iter()
-        .flat_map(|tx| {
-            tx.calls()
-                .iter()
-                .map(|call| call.code_hash)
-                .into_iter()
-                .map(|code_hash| {
-                    let mut bytecode = Bytecode::new(
-                        builder
-                            .code_db
-                            .0
-                            .get(&code_hash)
-                            .cloned()
-                            .expect("code db should has contain the code"),
-                    );
-                    bytecode.hash = U256::from_big_endian(code_hash.as_bytes());
-                    (bytecode.hash, bytecode)
-                })
-        })
-        .collect();
-
     Ok(witness_block)
 }
 
@@ -126,6 +101,74 @@ pub fn decode_bytecode(bytecode: &str) -> Result<Vec<u8>, anyhow::Error> {
     }
 
     hex::decode(stripped).map_err(|e| e.into())
+}
+
+const POSEIDONHASH_BYTES_IN_FIELD: usize = 16;
+
+#[derive(Debug, Clone)]
+struct PoseidonCodeHash {
+    bytes_in_field: usize,
+}
+
+impl PoseidonCodeHash {
+    fn new(bytes_in_field: usize) -> Self {
+        Self { bytes_in_field }
+    }
+}
+
+impl CodeHash for PoseidonCodeHash {
+    fn hash_code(&self, code: &[u8]) -> Hash {
+        let fls = (0..(code.len() / self.bytes_in_field))
+            .map(|i| i * self.bytes_in_field)
+            .map(|i| {
+                let mut buf: [u8; 32] = [0; 32];
+                U256::from_big_endian(&code[i..i + self.bytes_in_field]).to_little_endian(&mut buf);
+                Fr::from_bytes(&buf).unwrap()
+            });
+        let msgs: Vec<_> = fls
+            .chain(if code.len() % self.bytes_in_field == 0 {
+                None
+            } else {
+                let last_code = &code[code.len() - code.len() % self.bytes_in_field..];
+                // pad to bytes_in_field
+                let mut last_buf = vec![0u8; self.bytes_in_field];
+                last_buf.as_mut_slice()[..last_code.len()].copy_from_slice(last_code);
+                let mut buf: [u8; 32] = [0; 32];
+                U256::from_big_endian(&last_buf).to_little_endian(&mut buf);
+                Some(Fr::from_bytes(&buf).unwrap())
+            })
+            .collect();
+
+        let h = Fr::hash_msg(&msgs, Some(code.len() as u64));
+
+        let mut buf: [u8; 32] = [0; 32];
+        U256::from_little_endian(h.to_repr().as_ref()).to_big_endian(&mut buf);
+        Hash::from_slice(&buf)
+    }
+}
+
+#[test]
+fn code_hashing() {
+    let code_hasher = PoseidonCodeHash::new(16);
+    let simple_byte: [u8; 1] = [0];
+    assert_eq!(
+        format!("{:?}", code_hasher.hash_code(&simple_byte)),
+        "0x0ee069e6aa796ef0e46cbd51d10468393d443a00f5affe72898d9ab62e335e16"
+    );
+
+    let simple_byte: [u8; 2] = [0, 1];
+    assert_eq!(
+        format!("{:?}", code_hasher.hash_code(&simple_byte)),
+        "0x26cd650aa0d0b9aada79f5f7c03c5961430c12a2142832789fc31a4188d762ff"
+    );
+
+    let example = "608060405234801561001057600080fd5b506004361061004c5760003560e01c806321848c46146100515780632e64cec11461006d578063b0f2b72a1461008b578063f3417673146100a7575b600080fd5b61006b60048036038101906100669190610116565b6100c5565b005b6100756100da565b604051610082919061014e565b60405180910390f35b6100a560048036038101906100a09190610116565b6100e3565b005b6100af6100ed565b6040516100bc919061014e565b60405180910390f35b8060008190555060006100d757600080fd5b50565b60008054905090565b8060008190555050565b6000806100f957600080fd5b600054905090565b60008135905061011081610173565b92915050565b60006020828403121561012857600080fd5b600061013684828501610101565b91505092915050565b61014881610169565b82525050565b6000602082019050610163600083018461013f565b92915050565b6000819050919050565b61017c81610169565b811461018757600080fd5b5056fea2646970667358221220f4bca934426c76c7cb87cc32876fc6e65d1d7de23424faa61c347ffed95c449064736f6c63430008040033";
+    let bytes = hex::decode(example).unwrap();
+
+    assert_eq!(
+        format!("{:?}", code_hasher.hash_code(&bytes)),
+        "0x0e6d089fa72b508b90e014b486d64a5311df3030c45b10a95366cf53cd1ec9d5"
+    );
 }
 
 /*
@@ -164,37 +207,27 @@ fn get_account_created_codehash(step: &ExecStep) -> Result<eth_types::H256, anyh
     }
 }
 */
-fn trace_code(
-    cdb: &mut CodeDB,
-    step: &ExecStep,
-    sdb: &StateDB,
-    code: Bytes,
-    stack_pos: usize,
-) -> Result<(), anyhow::Error> {
+fn trace_code(cdb: &mut CodeDB, step: &ExecStep, sdb: &StateDB, code: Bytes, stack_pos: usize) {
     let stack = step
         .stack
         .as_ref()
         .expect("should have stack in call context");
     let addr = stack[stack.len() - stack_pos - 1].to_address(); //stack N-stack_pos
 
+    let hash = cdb.insert(code.to_vec());
+
+    // sanity check
     let (existed, data) = sdb.get_account(&addr);
-    if !existed {
-        // we may call non-contract or non-exist address
-        if code.as_ref().is_empty() {
-            Ok(())
-        } else {
-            Err(anyhow!("missed account data for {}", addr))
-        }
-    } else {
-        cdb.0.insert(data.code_hash, code.to_vec());
-        Ok(())
-    }
+    if existed {
+        assert_eq!(hash, data.code_hash);
+    };
 }
 pub fn build_statedb_and_codedb(
     blocks: &[BlockResult],
 ) -> Result<(StateDB, CodeDB), anyhow::Error> {
     let mut sdb = StateDB::new();
-    let mut cdb = CodeDB::new();
+    let mut cdb =
+        CodeDB::new_with_code_hasher(Box::new(PoseidonCodeHash::new(POSEIDONHASH_BYTES_IN_FIELD)));
 
     // step1: insert proof into statedb
     for block in blocks.iter().rev() {
@@ -210,8 +243,7 @@ pub fn build_statedb_and_codedb(
                     acc_mut.code_hash = acc.data.code_hash;
                     acc_mut.balance = acc.data.balance;
                 } else {
-                    // TODO(@noel2004): please complete the comment below.
-                    // only
+                    // it is essential to set it as default (i.e. not existed account data)
                     sdb.set_account(
                         addr,
                         Account {
@@ -274,17 +306,15 @@ pub fn build_statedb_and_codedb(
                         | OpcodeId::DELEGATECALL
                         | OpcodeId::STATICCALL => {
                             let callee_code = data.get_code_at(1);
-                            trace_code(&mut cdb, step, &sdb, callee_code, 1)?;
+                            trace_code(&mut cdb, step, &sdb, callee_code, 1);
                         }
                         OpcodeId::CREATE | OpcodeId::CREATE2 => {
-                            // TODO: need to insert code to codedb with poseidon codehash
-                            // if the call is persistent
+                            // notice we do not need to insert code for CREATE,
+                            // bustmapping do this job
                         }
                         OpcodeId::EXTCODESIZE | OpcodeId::EXTCODECOPY => {
                             let code = data.get_code_at(0);
-                            trace_code(&mut cdb, step, &sdb, code, 0).unwrap_or_else(|err| {
-                                log::error!("temporarily skip error in EXTCODE op: {}", err);
-                            });
+                            trace_code(&mut cdb, step, &sdb, code, 0);
                         }
 
                         _ => {}

neha-nupoor/scroll-zkevm

zkevm/src/circuit/builder.rs
@@ -1,9 +1,9 @@
 use super::{mpt, MAX_CALLDATA, MAX_RWS, MAX_TXS};
 use crate::circuit::{TargetCircuit, AUTO_TRUNCATE, DEGREE, MAX_INNER_BLOCKS, MAX_KECCAK_ROWS};
 use bus_mapping::circuit_input_builder::{self, BlockHead, CircuitInputBuilder, CircuitsParams};
-use bus_mapping::state_db::{Account, CodeDB, StateDB};
+use bus_mapping::state_db::{Account, CodeDB, CodeHash, StateDB};
 use eth_types::evm_types::OpcodeId;
-use eth_types::ToAddress;
+use eth_types::{Hash, ToAddress};
 use ethers_core::types::{Address, Bytes, U256};
 use types::eth::{BlockTrace, EthBlock, ExecStep};
 
@@ -12,7 +12,7 @@ use mpt_zktrie::state::ZktrieState;
 use zkevm_circuits::util::SubCircuit;
 use zkevm_circuits::evm_circuit::witness::block_apply_mpt_state;
 use zkevm_circuits::evm_circuit::witness::{block_convert, Block};
-
+use zkevm_circuits::bytecode_circuit::bytecode_unroller::HASHBLOCK_BYTES_IN_FIELD;
 
 use halo2_proofs::arithmetic::FieldExt;
 use halo2_proofs::halo2curves::bn256::Fr;
@@ -281,6 +281,74 @@ pub fn decode_bytecode(bytecode: &str) -> Result<Vec<u8>, anyhow::Error> {
     hex::decode(stripped).map_err(|e| e.into())
 }
 
+#[derive(Debug, Clone)]
+struct PoseidonCodeHash {
+    bytes_in_field: usize,
+}
+
+impl PoseidonCodeHash {
+    fn new(bytes_in_field: usize) -> Self {
+        Self { bytes_in_field }
+    }
+}
+
+impl CodeHash for PoseidonCodeHash {
+    fn hash_code(&self, code: &[u8]) -> Hash {
+        use mpt_zktrie::hash::MessageHashable;
+        use halo2_proofs::halo2curves::group::ff::PrimeField;
+        let fls = (0..(code.len() / self.bytes_in_field))
+            .map(|i| i * self.bytes_in_field)
+            .map(|i| {
+                let mut buf: [u8; 32] = [0; 32];
+                U256::from_big_endian(&code[i..i + self.bytes_in_field]).to_little_endian(&mut buf);
+                Fr::from_bytes(&buf).unwrap()
+            });
+        let msgs: Vec<_> = fls
+            .chain(if code.len() % self.bytes_in_field == 0 {
+                None
+            } else {
+                let last_code = &code[code.len() - code.len() % self.bytes_in_field..];
+                // pad to bytes_in_field
+                let mut last_buf = vec![0u8; self.bytes_in_field];
+                last_buf.as_mut_slice()[..last_code.len()].copy_from_slice(last_code);
+                let mut buf: [u8; 32] = [0; 32];
+                U256::from_big_endian(&last_buf).to_little_endian(&mut buf);
+                Some(Fr::from_bytes(&buf).unwrap())
+            })
+            .collect();
+
+        let h = Fr::hash_msg(&msgs, Some(code.len() as u64));
+
+        let mut buf: [u8; 32] = [0; 32];
+        U256::from_little_endian(h.to_repr().as_ref()).to_big_endian(&mut buf);
+        Hash::from_slice(&buf)
+    }
+}
+
+#[test]
+fn code_hashing() {
+    let code_hasher = PoseidonCodeHash::new(16);
+    let simple_byte: [u8; 1] = [0];
+    assert_eq!(
+        format!("{:?}", code_hasher.hash_code(&simple_byte)),
+        "0x0ee069e6aa796ef0e46cbd51d10468393d443a00f5affe72898d9ab62e335e16"
+    );
+
+    let simple_byte: [u8; 2] = [0, 1];
+    assert_eq!(
+        format!("{:?}", code_hasher.hash_code(&simple_byte)),
+        "0x26cd650aa0d0b9aada79f5f7c03c5961430c12a2142832789fc31a4188d762ff"
+    );
+
+    let example = "608060405234801561001057600080fd5b506004361061004c5760003560e01c806321848c46146100515780632e64cec11461006d578063b0f2b72a1461008b578063f3417673146100a7575b600080fd5b61006b60048036038101906100669190610116565b6100c5565b005b6100756100da565b604051610082919061014e565b60405180910390f35b6100a560048036038101906100a09190610116565b6100e3565b005b6100af6100ed565b6040516100bc919061014e565b60405180910390f35b8060008190555060006100d757600080fd5b50565b60008054905090565b8060008190555050565b6000806100f957600080fd5b600054905090565b60008135905061011081610173565b92915050565b60006020828403121561012857600080fd5b600061013684828501610101565b91505092915050565b61014881610169565b82525050565b6000602082019050610163600083018461013f565b92915050565b6000819050919050565b61017c81610169565b811461018757600080fd5b5056fea2646970667358221220f4bca934426c76c7cb87cc32876fc6e65d1d7de23424faa61c347ffed95c449064736f6c63430008040033";
+    let bytes = hex::decode(example).unwrap();
+
+    assert_eq!(
+        format!("{:?}", code_hasher.hash_code(&bytes)),
+        "0x0e6d089fa72b508b90e014b486d64a5311df3030c45b10a95366cf53cd1ec9d5"
+    );
+}
+
 /*
 fn get_account_deployed_codehash(
     execution_result: &ExecutionResult,
@@ -317,35 +385,25 @@ fn get_account_created_codehash(step: &ExecStep) -> Result<eth_types::H256, anyh
     }
 }
 */
-fn trace_code(
-    cdb: &mut CodeDB,
-    step: &ExecStep,
-    sdb: &StateDB,
-    code: Bytes,
-    stack_pos: usize,
-) -> Result<(), anyhow::Error> {
+fn trace_code(cdb: &mut CodeDB, step: &ExecStep, sdb: &StateDB, code: Bytes, stack_pos: usize) {
     let stack = step
         .stack
         .as_ref()
         .expect("should have stack in call context");
     let addr = stack[stack.len() - stack_pos - 1].to_address(); //stack N-stack_pos
 
+    let hash = cdb.insert(code.to_vec());
+
+    // sanity check
     let (existed, data) = sdb.get_account(&addr);
-    if !existed {
-        // we may call non-contract or non-exist address
-        if code.as_ref().is_empty() {
-            Ok(())
-        } else {
-            Err(anyhow!("missed account data for {}", addr))
-        }
-    } else {
-        cdb.0.insert(data.code_hash, code.to_vec());
-        Ok(())
-    }
+    if existed {
+        assert_eq!(hash, data.code_hash);
+    };
 }
 pub fn build_statedb_and_codedb(blocks: &[BlockTrace]) -> Result<(StateDB, CodeDB), anyhow::Error> {
     let mut sdb = StateDB::new();
-    let mut cdb = CodeDB::new();
+    let mut cdb =
+        CodeDB::new_with_code_hasher(Box::new(PoseidonCodeHash::new(HASHBLOCK_BYTES_IN_FIELD)));
 
     // step1: insert proof into statedb
     for block in blocks.iter().rev() {
@@ -361,8 +419,7 @@ pub fn build_statedb_and_codedb(blocks: &[BlockTrace]) -> Result<(StateDB, CodeD
                     acc_mut.code_hash = acc.data.code_hash;
                     acc_mut.balance = acc.data.balance;
                 } else {
-                    // TODO(@noel2004): please complete the comment below.
-                    // only
+                    // it is essential to set it as default (i.e. not existed account data)
                     sdb.set_account(
                         addr,
                         Account {
@@ -425,17 +482,15 @@ pub fn build_statedb_and_codedb(blocks: &[BlockTrace]) -> Result<(StateDB, CodeD
                         | OpcodeId::DELEGATECALL
                         | OpcodeId::STATICCALL => {
                             let callee_code = data.get_code_at(1);
-                            trace_code(&mut cdb, step, &sdb, callee_code, 1)?;
+                            trace_code(&mut cdb, step, &sdb, callee_code, 1);
                         }
                         OpcodeId::CREATE | OpcodeId::CREATE2 => {
-                            // TODO: need to insert code to codedb with poseidon codehash
-                            // if the call is persistent
+                            // notice we do not need to insert code for CREATE,
+                            // bustmapping do this job
                         }
                         OpcodeId::EXTCODESIZE | OpcodeId::EXTCODECOPY => {
                             let code = data.get_code_at(0);
-                            trace_code(&mut cdb, step, &sdb, code, 0).unwrap_or_else(|err| {
-                                log::error!("temporarily skip error in EXTCODE op: {}", err);
-                            });
+                            trace_code(&mut cdb, step, &sdb, code, 0);
                         }
 
                         _ => {}